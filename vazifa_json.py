a1c9c31347c697b85f7582651bf5238cd1e6bc86
sevinchkarimboyeva <sevinchkarimboyeva52@gmail.com>
2025-10-03 20:56:17 +0500

    Create vazifa_json.py

---
+++ vazifa_json.py
@@ -0,0 +1,46 @@
+import json
+data = {
+    "Model" : "Malibu",
+    "Rang" : "Qora",
+    "Yil":2020,
+    "Narh":40000
+}
+data_json=json.dumps(data)
+print(data_json)
+
+talaba= {
+    "ism":"Hasan",
+    "familiya":"Husanov",
+    "tyil":2000
+}
+talaba_json=json.dumps(talaba)
+print(f"Talabaning ismi:{talaba['ism']}")
+print(f"Talabaning familiyasi:{talaba['familiya']}")
+
+with open('data.json','w') as d:
+    json.dump(data,d)
+
+with open('talaba.json','w') as t:
+    json.dump(talaba,t)
+
+
+
+with open('talabalar.json','r') as t_j:
+    talaba=json.load(t_j)
+
+talabalar_list=talaba["student"]
+for student in talabalar_list:
+    id=student['id']
+    name=student['name']
+    lastname=student['lastname']
+    year=student['year']
+    faculty=student['faculty']
+
+print(f"Id:{id},\nIsm-Familiyasi:{name} {lastname},\nYili:{year} \nFakulteti:{faculty}")
+
+
+
+
+
+
+
